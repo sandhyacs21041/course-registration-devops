f69bbc1253168534ba9dcff2f2c158e8927c2c5b
Sandhya <sandhyahmotur@gmail.com>
2026-03-29 22:25:08 +0530

    Updated students page UI and API fix

--- backend/src/main/java/com/courseapp/controller/StudentController.java
+++ backend/src/main/java/com/courseapp/controller/StudentController.java
@@ -9,9 +9,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/api/students")
-@CrossOrigin(origins = "*")
 public class StudentController {
 
     @Autowired
